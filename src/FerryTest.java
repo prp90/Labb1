5249368925090cff0c251d9d83350e4696cacbf0
PriscilaPrado <p.prado90@gmail.com>
2015-11-18 12:25:03 +0100

    RED - FerryTest setUp()
    setUp the test object. RED since there is no tests (yet!) in FerryTest

--- src/FerryTest.java
+++ src/FerryTest.java
@@ -1,18 +1,15 @@
 import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
 
 
-public class FerryTest {
+public class FerryTest extends TestCase {
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+		Ferry testFerryObject = new Ferry();
 	}
 
 }
