5533f23505452dbda36e8827439098da1d90684c
antonmosen <antonmosenbacher@hotmail.com>
2015-11-19 09:26:26 +0100

    REFACTOR - Test Ferry Space For Passenger Should Return True
    acceptingNewPassengers() has been refactored.

--- src/Ferry.java
+++ src/Ferry.java
@@ -2,14 +2,16 @@
 public class Ferry {
 
 	private int vehicleSpace = 0;
+	private final int MAX_PASSENGERS = 200;
+	private int passengers = 0;
 	
 	public boolean isEmpty() {
 		return vehicleSpace == 0? true: false;
 	}
 
 	public boolean acceptingNewPassenger() {
-		
-		return true;
+	
+		return passengers < MAX_PASSENGERS;
 	}
 
 
